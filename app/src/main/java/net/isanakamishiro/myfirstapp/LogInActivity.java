e04346a74e49902ace1919798e441f5afe0549de
isana <isana_kamishiro@yahoo.co.jp>
2017-09-10 16:44:04 +0900

    Enable Google signIn

--- app/src/main/java/net/isanakamishiro/myfirstapp/LogInActivity.java
+++ app/src/main/java/net/isanakamishiro/myfirstapp/LogInActivity.java
@@ -1,30 +1,45 @@
 package net.isanakamishiro.myfirstapp;
 
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.annotation.VisibleForTesting;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import net.isanakamishiro.myfirstapp.util.Alert;
 
 public class LogInActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 9001;
 
     EditText emailEditText;
     EditText passwordEditText;
     Button logInButton;
     TextView signUpTextView;
     private FirebaseAuth mFirebaseAuth;
+    private GoogleApiClient mGoogleApiClient;
 
+    @VisibleForTesting
+    public ProgressDialog mProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +52,8 @@ public class LogInActivity extends AppCompatActivity {
         emailEditText = findViewById(R.id.emailField);
         passwordEditText = findViewById(R.id.passwordField);
         logInButton = findViewById(R.id.loginButton);
+
+        configureGoogleSignIn();
 
         signUpTextView.setOnClickListener(v -> {
             Intent intent = new Intent(LogInActivity.this, SignUpActivity.class);
@@ -59,8 +76,11 @@ public class LogInActivity extends AppCompatActivity {
                             R.string.login_error_title);
 
                 } else {
+                    showProgressDialog();
                     mFirebaseAuth.signInWithEmailAndPassword(email, password)
                             .addOnCompleteListener(LogInActivity.this, task -> {
+                                hideProgressDialog();
+
                                 if (task.isSuccessful()) {
                                     placeToMain();
                                 } else {
@@ -71,14 +91,92 @@ public class LogInActivity extends AppCompatActivity {
                             });
                 }
             }
-
-            private void placeToMain() {
-                Intent intent = new Intent(LogInActivity.this, MainActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                startActivity(intent);
-            }
         });
 
     }
+
+    private void configureGoogleSignIn() {
+
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, connectionResult -> failedGoogleSignIn())
+                .addApi(Auth.GOOGLE_SIGN_IN_API, googleSignInOptions)
+                .build();
+
+        findViewById(R.id.google_sign_in_button).setOnClickListener(view -> {
+            Intent intent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+            startActivityForResult(intent, RC_SIGN_IN);
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        if (result.isSuccess()) {
+            GoogleSignInAccount account = result.getSignInAccount();
+
+            showProgressDialog();
+
+            AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+            mFirebaseAuth.signInWithCredential(credential).addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    placeToMain();
+                } else {
+                    Toast.makeText(LogInActivity.this,
+                            R.string.authentication_failed, Toast.LENGTH_SHORT).show();
+                }
+
+                hideProgressDialog();
+            });
+        }
+
+    }
+
+    private void failedGoogleSignIn() {
+
+    }
+
+    private void placeToMain() {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
+    public void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage(getString(R.string.loading));
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        hideProgressDialog();
+    }
+
+
 }
--- app/src/main/java/net/isanakamishiro/myfirstapp/MainActivity.java
+++ app/src/main/java/net/isanakamishiro/myfirstapp/MainActivity.java
@@ -32,9 +32,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        Toolbar toolbar = findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-
         mFirebaseAuth = FirebaseAuth.getInstance();
         mFirebaseUser = mFirebaseAuth.getCurrentUser();
         mDatabase = FirebaseDatabase.getInstance().getReference();
