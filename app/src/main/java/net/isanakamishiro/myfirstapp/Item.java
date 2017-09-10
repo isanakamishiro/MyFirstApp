d957185e20a32dcfd943a51eecfa488131e6485f
isana <isana_kamishiro@yahoo.co.jp>
2017-09-10 15:11:39 +0900

    lombok implemented

--- app/src/main/java/net/isanakamishiro/myfirstapp/Item.java
+++ app/src/main/java/net/isanakamishiro/myfirstapp/Item.java
@@ -1,21 +1,14 @@
 package net.isanakamishiro.myfirstapp;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 /**
  * Created by isana on 2017/09/09.
  */
 
+@Data
+@AllArgsConstructor
 public class Item {
     private String title;
-
-    public Item(String s) {
-        this.title = s;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
 }
